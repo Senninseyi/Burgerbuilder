39a0bf93b73f9eb6932ee875dba1d2e63fec83f1
Senninseyi <seyi.oyebamiji@gmail.com>
2021-09-24 18:31:25 +0100

    Update order.js

--- src/burgerbuilder/components/order/order.js
+++ src/burgerbuilder/components/order/order.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const Order = styled.div`
-    width:100%;
+    width:80%;
     border:1px solid #ccc;
     box-shadow: 0 2px 3px #ccc;
     padding: 10px;
@@ -10,11 +10,39 @@ const Order = styled.div`
     box-sizing: border-box;
 `
 
-const order = (props) => (
-    <Order>
-        <p>Ingredients: Salad(1)</p>
-        <p>Price: <strong>USD 5.45</strong> </p>
-    </Order>
-)
+const order = (props) => {
+
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push(
+            {
+                name: ingredientName,
+                amount: props.ingredients[ingredientName]
+            }
+        )
+    }
+
+    const ingredientsOutput = ingredients.map(ig => {
+        return <span 
+                    key={ig.name}
+                    style={{
+                        textTransform: 'capitalize',
+                        display: 'inline-block',
+                        margin: '0 8px',
+                        border: '1px solid #ccc',
+                        padding: '5px'
+                    }}>
+                        {ig.name} {ig.amount}
+               </span>
+    })
+
+    return(
+        <Order>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)} </strong> </p>
+        </Order>    
+    )
+}
 
 export default order
