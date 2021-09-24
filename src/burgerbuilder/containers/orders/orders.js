cb7cd53e4fc5fe5fda7e075be57103b3f95cab18
Senninseyi <seyi.oyebamiji@gmail.com>
2021-09-24 18:31:33 +0100

    Update orders.js

--- src/burgerbuilder/containers/orders/orders.js
+++ src/burgerbuilder/containers/orders/orders.js
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
 import Order from "../../components/order/order";
+import axios from "../../../axios/axios-orders";
+import withErrorHandler from "../../withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        isLoading: true,
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+        .then(res => {
+            const fetchedOrder = []
+            for (let key in res.data) {
+                fetchedOrder.push({
+                    ...res.data[key],
+                    id: key,
+                })
+            }
+            this.setState({isLoading:false, orders:fetchedOrder})
+        })
+            .catch( error => {
+                this.setState({isLoading:false})
+            }
+        )
+    }
+
     render(){
         return(
             <div>
-                <Order/>
-                <Order/>
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={+order.price}/>
+                ))}
             </div>
         )
     }
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios)
