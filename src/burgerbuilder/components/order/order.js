72885cef2798bde467537d22ccb3f47f7abec91d
Senninseyi <seyi.oyebamiji@gmail.com>
2021-09-24 13:16:15 +0100

    Update order.js

--- src/burgerbuilder/components/order/order.js
+++ src/burgerbuilder/components/order/order.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Order = styled.div`
+    width:100%;
+    border:1px solid #ccc;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 10px;
+    margin: 10px auto;
+    box-sizing: border-box;
+`
 
 const order = (props) => (
-    <div>
+    <Order>
         <p>Ingredients: Salad(1)</p>
         <p>Price: <strong>USD 5.45</strong> </p>
-    </div>
+    </Order>
 )
 
 export default order
