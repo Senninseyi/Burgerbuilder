e907c4b2b61c280c0d064c039c321bf34bf0f299
Senninseyi <seyi.oyebamiji@gmail.com>
2021-09-24 12:44:18 +0100

    Update checkout.js
    updated the props routing on the checkout page

--- src/burgerbuilder/containers/checkout/checkout.js
+++ src/burgerbuilder/containers/checkout/checkout.js
@@ -42,7 +42,8 @@ class Checkout extends Component {
                     checkoutContinue={this.checkoutContinueHandler}/>
                 <Route 
                     path={this.props.match.path + '/contact-data'} 
-                    render={() => (<ContactData ingredient={this.state.ingredients} price={this.state.totalPrice} />)} />
+                    render={(props) => 
+                        (<ContactData ingredient={this.state.ingredients} price={this.state.totalPrice} {...props} />)}/>
             </div>
         )
     }
