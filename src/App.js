bcc38cfb51b8fef74d859e6a4c111dbd663e2895
Senninseyi <seyi.oyebamiji@gmail.com>
2022-01-29 21:17:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,29 +3,66 @@ import Layout from './burgerbuilder/components/layout/layout';
 import './App.css';
 import BurgerBuilder from './burgerbuilder/containers/burgerbuilder/burgerbuilder';
 // import Blog from './blogpost/containers/Blog/Blog.js';
-import {BrowserRouter, Route, Switch} from "react-router-dom"
+import { Route, Switch, withRouter} from "react-router-dom"
 import Checkout from './burgerbuilder/containers/checkout/checkout';
 import Orders from './burgerbuilder/containers/orders/orders';
 import Auth from './burgerbuilder/containers/auth/auth';
+import Logout from './burgerbuilder/containers/auth/logout/logout';
+import { connect } from 'react-redux';
+
+import * as actions from './store/actions/index'
+import { Redirect } from 'react-router-dom';
 
 class App extends Component {
+
+  componentDidMount(){
+    this.props.onTryAutoSignUp()
+  }
+
   render(){
+
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth}/>
+        <Route path="/" exact component={BurgerBuilder}/>
+        <Redirect to='/'/>
+      </Switch>
+    )
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/orders" component={Orders}/>
+          <Route path="/logout" component={Logout}/>
+          <Route path="/auth" component={Auth}/>
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Redirect to='/'/>
+        </Switch>
+      )
+    }
+
     return(
-      <BrowserRouter>
-        <div className="App">
-          <Layout>
-            <Switch>
-              <Route path="/checkout" component={Checkout}/>
-              <Route path="/orders" component={Orders}/>
-              <Route path="/auth" component={Auth}/>
-              <Route path="/" exact component={BurgerBuilder}/>
-            </Switch>
-          </Layout>
-          {/* <Blog/> */}
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <Layout>
+          {routes}
+        </Layout>
+        {/* <Blog/> */}
+      </div>
     )
   }
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignUp: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default withRouter((connect(mapStateToProps, mapDispatchToProps))(App));
