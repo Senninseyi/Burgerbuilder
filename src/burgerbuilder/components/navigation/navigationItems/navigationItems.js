eedd6710399ae572b2be15c872f11db7ca4cb95d
Senninseyi <seyi.oyebamiji@gmail.com>
2022-01-29 21:18:22 +0100

    Update navigationItems.js

--- src/burgerbuilder/components/navigation/navigationItems/navigationItems.js
+++ src/burgerbuilder/components/navigation/navigationItems/navigationItems.js
@@ -6,11 +6,17 @@ const NavigationItems = tw.ul`
     m-0 p-2 lg:p-0 md:p-0 list-none flex items-center h-full flex-col lg:flex-row md:flex-row
 `
 
-const navigationItems = () => (
+const navigationItems = (props) => (
     <NavigationItems>
         <NavigationItem link="/" exact>BurgerBuilder</NavigationItem>
-        <NavigationItem link="/orders">Orders</NavigationItem>
-        <NavigationItem link="/auth">Authenticate</NavigationItem>
+        {   props.isAuthenticated 
+            ?<NavigationItem link="/orders">Orders</NavigationItem>
+            : null
+        }
+        {   !props.isAuthenticated 
+            ? <NavigationItem link="/auth">Authenticate</NavigationItem>
+            : <NavigationItem link="/logout">LogOut</NavigationItem>
+        }
     </NavigationItems>
 );
 
